678197108bb43ca4cbace614aee74fb11c9b767b
joaopaulofg <joaofpaulo1@gmail.com>
2025-05-12 10:02:10 -0300

    configuracoes iniciais do RabbitMQ

---
+++ auth-service/src/main/java/com/voting/auth/config/RabbitMQConfiguration.java
@@ -0,0 +1,50 @@
+package com.voting.auth.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfiguration {
+
+    @Value("${rabbitmq.novousuario.exchange}")
+    private String exchangeNovoUsuario;
+
+    @Bean
+    public Queue criarFilaNovoUsurioMsNotificacao() {
+        return QueueBuilder.durable("novo-usuario.ms-notificacao").build();
+    }
+
+    @Bean
+    public RabbitAdmin criarRabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public FanoutExchange criarFanoutNovoUsuario() {
+        return ExchangeBuilder.fanoutExchange(exchangeNovoUsuario).build();
+    }
+
+    @Bean
+    public Binding criarBindingNovoUsuarioMsNotificacao() {
+        return BindingBuilder.bind(criarFilaNovoUsurioMsNotificacao()).to(criarFanoutNovoUsuario());
+    }
+
+    @Bean
+    public RabbitTemplate criarRabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setConnectionFactory(connectionFactory);
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    public MessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
