a4387487cbb5d484f8f7e8d5e582db6edf4f8077
joaopaulofg <joaofpaulo1@gmail.com>
2025-05-13 09:22:15 -0300

    configurando rabbitmq no segundo ms

--- voting-service/src/main/java/com/voting/votingservice/config/RabbitMQConfiguration.java
+++ voting-service/src/main/java/com/voting/votingservice/config/RabbitMQConfiguration.java
@@ -1,15 +1,55 @@
 package com.voting.votingservice.config;
 
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfiguration {
 
+    @Value("${rabbitmq.novousuario.exchange}")
+    private String exchangeNovoUsuario;
+
+    @Value("${rabbitmq.novousuario.queue}")
+    private String queueNovoUsuario;
+
     @Bean
-    public MessageConverter messageConverter() {
+    public Queue criarFilaNovoUsuarioMsNotificacao() {
+        return QueueBuilder.durable(queueNovoUsuario).build();
+    }
+
+    @Bean
+    public RabbitAdmin criarRabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public FanoutExchange criarFanoutNovoUsuario() {
+        return ExchangeBuilder.fanoutExchange(exchangeNovoUsuario).build();
+    }
+
+    @Bean
+    public Binding criarBindingNovoUsuarioMsNotificacao() {
+        return BindingBuilder.bind(criarFilaNovoUsuarioMsNotificacao()).to(criarFanoutNovoUsuario());
+    }
+
+    @Bean
+    public RabbitTemplate criarRabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setConnectionFactory(connectionFactory);
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public MessageConverter jackson2JsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 }
+
